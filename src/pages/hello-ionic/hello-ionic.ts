4feac977b6e8c814048003d5629b2e5a3c52acaa
sajikuttan <sajikuttan1992@gmail.com>
2017-07-28 02:43:00 +0530

    Added Languges Page

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -16,17 +16,24 @@ import { ListPage } from '../list/list';
 export class HelloIonicPage {
   shownGroup = null;
   technologies = [];
+  data = ['PHP','JAVA','MYSQL'];
   constructor(public navCtrl: NavController, public navParams: NavParams, public videoPlayer: VideoPlayer, public dom : DomSanitizer,public http: Http) {
-    this.http.get('http://localhost/ElearnApp/technology.php')
-    .map(res => res.json())
-    .subscribe(data => {
-        for(var i =0;i<data.length;i++){
-          this.technologies.push({
-              id: data[i].id,
-              technology:data[i].technology
-          });
-        }
-      });
+    // this.http.get('http://localhost/ElearnApp/technology.php')
+    // .map(res => res.json())
+    // .subscribe(data => {
+    //     for(var i =0;i<data.length;i++){
+    //       this.technologies.push({
+    //           id: data[i].id,
+    //           technology:data[i].technology
+    //       });
+    //     }
+    //   });
+    for(var i =0;i<this.data.length;i++){
+        this.technologies.push({
+            id: i,
+            technology: this.data[i]
+        });
+    }
   }
   itemTapped(event, item) {
     this.navCtrl.push(ItemDetailsPage, {
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -15,17 +15,35 @@ export class ListPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
   technologies = [];
+  technologies2 = [];
+  language : any;
+  data = ['PHP','JAVA','MYSQL'];
+  data2 = ['ANGULAR','TYPESCRIPT','ASP','TESTING','CPP','C','PYTHON','RUBY'];
   constructor(public navCtrl: NavController, public navParams: NavParams,public http: Http) {
-    this.http.get('http://localhost/ElearnApp/technology.php')
-    .map(res => res.json())
-    .subscribe(data => {
-        for(var i =0;i<data.length;i++){
-          this.technologies.push({
-              id: data[i].id,
-              technology:data[i].technology
-          });
-        }
-      });
+    this.language = "selected-language";
+    // this.http.get('http://localhost/ElearnApp/technology.php')
+    // .map(res => res.json())
+    // .subscribe(data => {
+    //     for(var i =0;i<data.length;i++){
+    //       this.technologies.push({
+    //           id: data[i].id,
+    //           technology:data[i].technology
+    //       });
+    //     }
+    //   });
+    var i:any;
+    for(i =0;i<this.data.length;i++){
+        this.technologies.push({
+            id: i,
+            technology: this.data[i]
+        });
+    }
+    for(i =0;i<this.data2.length;i++){
+        this.technologies2.push({
+            id: i,
+            technology: this.data2[i]
+        });
+    }
   }
 
   itemTapped(event, item) {
